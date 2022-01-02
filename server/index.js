b6b67c3e471c0c886142c0a8f0e971cba848ecee
unknown <anand.mishra@oodles.io>
2022-01-02 09:31:10 +0530

    project-initialized

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import bodyParser from "body-parser";
+import express from "express";
+import  mongoose  from "mongoose";
+import cors from 'cors';
+
+const app= express();
+app.use(bodyParser.json({limit:'30mb',extended:'true'}))
+app.use(bodyParser.urlencoded({limit:'30mb',extended:'true'}))
+app.use(cors());
+const CONNECTION_URL='mongodb+srv://anandmishra71996:anand%40123@cluster0.ypq9k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT= process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server is running on port ${PORT}`);
+    })
+}).catch((err)=>{
+    console.log(err);
+})
